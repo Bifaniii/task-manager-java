f4beb027ca0da385106f27c99dd524adae347bc6
Bifani <gbifani.tech@gmail.com>
2026-03-31 14:46:25 -0300

    test: add initial test structure

---
+++ src/test/java/com/bifani/taskmanagerjava/TaskmanagerjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bifani.taskmanagerjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
