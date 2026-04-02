2db10c19e6f5a9752eb6015833477330c36d388d
Bifani <gbifani.tech@gmail.com>
2026-04-02 16:53:38 -0300

    feat: implement security base with Spring Security, UserDetails and ByCript

---
+++ src/main/java/com/bifani/taskmanagerjava/database/repository/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.bifani.taskmanagerjava.database.repository;
+
+import com.bifani.taskmanagerjava.database.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
+}
---
+++ src/main/java/com/bifani/taskmanagerjava/service/AuthorizationService.java
@@ -0,0 +1,20 @@
+package com.bifani.taskmanagerjava.service;
+
+import com.bifani.taskmanagerjava.database.repository.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    private final IUserRepository repository;
+
+    public AuthorizationService(IUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
