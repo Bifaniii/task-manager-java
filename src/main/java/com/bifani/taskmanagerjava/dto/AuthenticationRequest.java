89e4a14adf24f1e4099c3520a2696f9f2cb61f82
Bifani <gbifani.tech@gmail.com>
2026-04-04 22:35:39 -0300

    feat: add missing authentication and register DTO records

---
+++ src/main/java/com/bifani/taskmanagerjava/dto/AuthenticationRequest.java
@@ -0,0 +1,3 @@
+package com.bifani.taskmanagerjava.dto;
+
+public record AuthenticationRequest (String email, String password){}
---
+++ src/main/java/com/bifani/taskmanagerjava/dto/RegisterRequest.java
@@ -0,0 +1,4 @@
+package com.bifani.taskmanagerjava.dto;
+
+public record RegisterRequest(String name, String email, String password) {
+}
