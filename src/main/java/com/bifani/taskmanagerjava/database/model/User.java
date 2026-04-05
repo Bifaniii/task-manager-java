3182f7612e3dbd694ef56dfa4e5502accc88e497
Bifani <gbifani.tech@gmail.com>
2026-04-04 22:22:20 -0300

    feat: implement UserDetails and security configuration

--- src/main/java/com/bifani/taskmanagerjava/database/model/User.java
+++ src/main/java/com/bifani/taskmanagerjava/database/model/User.java
@@ -3,6 +3,7 @@ package com.bifani.taskmanagerjava.database.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -30,7 +31,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
--- src/main/java/com/bifani/taskmanagerjava/service/AuthorizationService.java
+++ src/main/java/com/bifani/taskmanagerjava/service/AuthorizationService.java
@@ -4,7 +4,9 @@ import com.bifani.taskmanagerjava.database.repository.IUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthorizationService implements UserDetailsService {
 
     private final IUserRepository repository;
